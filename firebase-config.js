ccd9e2f06a67aaa2d31af39b67d61ff36e7718f7
korapat5456-ux <korapat5456@gmail.com>
2025-08-09 21:25:32 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9H3UlJ8M9G0P0JKr4FNaV4xpCme-JJYk",
+  authDomain: "dsnverse-project.firebaseapp.com",
+  projectId: "dsnverse-project",
+  storageBucket: "dsnverse-project.firebasestorage.app",
+  messagingSenderId: "1085114891216",
+  appId: "1:1085114891216:web:a7c179f462abc10edf57d5",
+  measurementId: "G-XWQC3L71L2"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
